9d1b027c741f33922ad515f3e76c25d29b1f55a3
Paul21212 <paul.zickermann@gmx.de>
2025-07-31 12:24:12 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BallGame from './BallGame';
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center' }}>Mini Ball Game</h1>
+      <BallGame />
+    </div>
+  );
+}
+
+export default App;
